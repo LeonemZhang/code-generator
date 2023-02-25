8348f0118a49100200fb12a32e7cbc1762b6500a
Magellan <maizlcareer@163.com>
2023-02-25 22:56:32 +0800

    添加electron

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./", // 设置相对路径的根路径
   resolve: {
     alias: {
       "@": "/src",
@@ -12,5 +13,12 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {},
     },
+  },
+  build: {
+    sourcemap: true,
+    target: "chrome92", // 必须是 Chrome 的某个版本，以便 Electron 可以使用
+  },
+  server: {
+    port: 3000, // 您的应用程序将在此端口上运行
   },
 });
